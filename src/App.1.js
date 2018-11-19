0ef862e719a98823d5c7e3a44f0f545791297406
Siraldo Jose <siraldo.bmx@gmail.com>
2018-11-19 19:12:08 -0200

    Done games

---
+++ src/App.1.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const value1 = Math.floor(Math.random() * 100);
+const value2 = Math.floor(Math.random() * 100);
+const value3 = Math.floor(Math.random() * 100);
+const proposedAnswer = Math.floor(Math.random() * 3) + value1 + value2 + value3;
+
+class App extends Component {
+  state = {
+    numQuestions: 0,
+    numCorrect: 0,
+  }
+
+  trueCount = (numCorrect) => {
+    this.setState((prevState) => ({
+      numCorrect: prevState.count + 1
+    }))
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">ReactND - Coding Practice</h1>
+        </header>
+        <div className="game">
+          <h2>Mental Math</h2>
+          <div className="equation">
+            <p className="text">{`${value1} + ${value2} + ${value3} = ${proposedAnswer}`}</p>
+          </div>
+          <button
+            onClick={() => this.props.trueCount(this.state.numCorrect)}
+            className='true-count'>
+              True
+          </button>
+          <button>False</button>
+          <p className="text">
+            Your Score: {this.state.numCorrect}/{this.state.numQuestions}
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
